6a84f5beb04519691f7b44f113cff0ba1c5e8559
Jean M Santos <jean.michel@ebz.tec.br>
2024-10-28 13:44:32 -0300

    class for generate user data

--- src/test/java/br/tec/ebz/connid/connector/cyberark/utils/GetUser.java
+++ src/test/java/br/tec/ebz/connid/connector/cyberark/utils/GetUser.java
@@ -1,4 +1,107 @@
 package br.tec.ebz.connid.connector.cyberark.utils;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 public class GetUser {
+
+    private int id;
+    private List<String> groups;
+    private String workStreet;
+    private String workCity;
+    private String workState;
+    private String workZip;
+    private String workCountry;
+    private long lastSuccessfullLoginDate;
+
+    public GetUser() {
+        id = new Random().nextInt(500);
+        lastSuccessfullLoginDate = new Date().getTime();
+
+        groups = new ArrayList<>();
+        groups.add("Group1");
+        groups.add("Group2");
+        groups.add("Group3");
+
+        workStreet = "Street Test";
+        workCity = "City Test";
+        workState = "State Test";
+        workZip = "00000-000";
+        workCountry = "Brazil";
+
+    }
+
+    public long getLastSuccessfullLoginDate() {
+        return lastSuccessfullLoginDate;
+    }
+
+    public void setLastSuccessfullLoginDate(long lastSuccessfullLoginDate) {
+        this.lastSuccessfullLoginDate = lastSuccessfullLoginDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWorkStreet() {
+        return workStreet;
+    }
+
+    public void setWorkStreet(String workStreet) {
+        this.workStreet = workStreet;
+    }
+
+    public String getWorkCity() {
+        return workCity;
+    }
+
+    public void setWorkCity(String workCity) {
+        this.workCity = workCity;
+    }
+
+    public String getWorkState() {
+        return workState;
+    }
+
+    public void setWorkState(String workState) {
+        this.workState = workState;
+    }
+
+    public String getWorkZip() {
+        return workZip;
+    }
+
+    public void setWorkZip(String workZip) {
+        this.workZip = workZip;
+    }
+
+    public String getWorkCountry() {
+        return workCountry;
+    }
+
+    public void setWorkCountry(String workCountry) {
+        this.workCountry = workCountry;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return "createTestUser" + id;
+    }
+
+    public String homeEmail() {
+        return "test@example.com";
+    }
+
+    public void setGroups(List<String> groups) {
+        this.groups = groups;
+    }
+
+    public List<String> getGroups() {
+        return groups;
+    }
 }
