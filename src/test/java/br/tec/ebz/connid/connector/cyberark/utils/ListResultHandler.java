7cb59ece82f649123f1a549c3f9f84d903daf89b
Jean M Santos <jean.michel@ebz.tec.br>
2024-10-28 19:31:26 -0300

    defined list result handler for tests

--- src/test/java/br/tec/ebz/connid/connector/cyberark/utils/ListResultHandler.java
+++ src/test/java/br/tec/ebz/connid/connector/cyberark/utils/ListResultHandler.java
@@ -1,4 +1,18 @@
 package br.tec.ebz.connid.connector.cyberark.utils;
 
-public class ListResultHandler {
+import org.identityconnectors.framework.common.objects.ConnectorObject;
+import org.identityconnectors.framework.common.objects.ResultsHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListResultHandler implements ResultsHandler {
+    private List<ConnectorObject> objects = new ArrayList<>();
+    @Override
+    public boolean handle(ConnectorObject connectorObject) {
+        objects.add(connectorObject);
+        return true;
+    }
+
+    public List<ConnectorObject> getObjects() { return objects; }
 }
