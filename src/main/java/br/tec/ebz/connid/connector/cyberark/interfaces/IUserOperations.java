c5f25ab28711265e587dc99f5d0b7d255ba6f9cb
Jean M Santos <jean.michel@ebz.tec.br>
2024-10-28 13:50:40 -0300

    changed to use unirest as dependency

--- src/main/java/br/tec/ebz/connid/connector/cyberark/interfaces/IUserOperations.java
+++ src/main/java/br/tec/ebz/connid/connector/cyberark/interfaces/IUserOperations.java
@@ -1,29 +1,36 @@
 package br.tec.ebz.connid.connector.cyberark.interfaces;
 
+import kong.unirest.core.json.JSONObject;
 import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.common.objects.*;
 import org.identityconnectors.framework.common.objects.filter.Filter;
-import org.json.JSONObject;
 
+import java.net.URISyntaxException;
 import java.util.Set;
 
-public interface IUserProcessing {
+public interface IUserOperations {
 
-    Uid create(Set<Attribute> attributeSet);
+    String type();
+
+    Uid create(Set<Attribute> attributeSet) throws URISyntaxException;
 
     void changePassword(String uid, GuardedString password);
 
-    void changeStatus(String uid, Boolean status);
+    void enableUser(String uid) throws URISyntaxException;
+
+    void disableUser(String uid) throws URISyntaxException;
+
+    void activateUser(String uid) throws URISyntaxException;
 
     String update(Uid uid, Set<AttributeDelta> deltas);
 
-    void delete(Uid uid);
+    void delete(Uid uid) throws URISyntaxException;
 
-    void search(Filter filter, ResultsHandler resultsHandler);
+    void search(Filter filter, ResultsHandler resultsHandler) throws URISyntaxException;
 
-    void searchAll(ResultsHandler resultsHandler);
+    void searchAll(ResultsHandler resultsHandler) throws URISyntaxException;
 
-    void searchByFilter(Filter filter, ResultsHandler resultsHandler);
+    void searchByFilter(Filter filter, ResultsHandler resultsHandler) throws URISyntaxException;
 
     ConnectorObject translateToConnectorObject(JSONObject user);
 
